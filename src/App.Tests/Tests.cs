84a2ff08d92e71b59c6f02efa461dc1c755cc0a1
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-05-22 10:41:26 -0400

    Update gitignore and test

--- src/App.Tests/Tests.cs
+++ src/App.Tests/Tests.cs
@@ -1,9 +1,17 @@
-namespace App.Tests;
+using Microsoft.Playwright.Xunit; // These two are very important
+using System.Threading.Tasks; // This is the async keyword
 
-public class CounterCSharpTests : TestContext
+namespace App.Tests; // will vary depending on project name
+
+// Name does not matter here
+// Notice how we extend PageTest, which is different than a normal bUnit or xUnit test.
+public class ReallyGoodTest() : PageTest
 {
-	[Fact]
-	public void EndToEndTestFail()
-	{
-	}
+    [Fact] // declares a method as a test
+    //Another name that does not matter
+    public async Task PageHasBody()
+    {
+        await Page.GotoAsync("http://localhost:5155");
+        await Expect(Page.Locator("body")).ToBeAttachedAsync();
+    }
 }
